6a0c27aece31827ef90e06d70c2e75c83c80cf51
NishNosh-design <78135240+NishNosh-design@users.noreply.github.com>
2023-09-21 21:03:20 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img = "";
 var  status = "";
+objects = [];
 
 function preload(){
     img = loadImage('dog_cat.jpg')
@@ -14,6 +15,23 @@ function setup() {
 
 function draw() {
     image(img, 0, 0, 640, 420);
+
+    if( status != "")
+    {
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y,  objects[i].width, objects[i].height);
+        }
+    }
+
+
     fill("#FF0000");
     text("Dog", 45, 75);
     noFill();
@@ -38,4 +56,5 @@ function gotResult(error, results) {
         console.log(error);
     }
     console.log(results);
+    objects = results;
 }
